ebbb2d571bb17c2768df95be70eea41e14d4baff
djowroots <43259672+djowroots@users.noreply.github.com>
2018-10-13 06:27:52 -0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,4 +1,4 @@
 USE `essentialmode`;
 
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-  ('headbag', 'Saco para Cabeça', 20)
+  ('headbag', 'Saco para Cabeça', 5) --   ('headbag', 'Head Bag', 5) en
